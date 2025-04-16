5fde4b765f4a90c16e601f93dd76a0cfdc745f0f
thedb <thedb11@gmail.com>
2025-04-16 12:32:42 +0200

    Simplified linker module.

--- modules/linker.nix
+++ modules/linker.nix
@@ -3,20 +3,10 @@
    # Enable dynamic linking.
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
-    # Bevy dependencies
     alsa-lib
-    alsa-utils
-    freetype
-    unstable.glibc.dev
-    unstable.gcc14
     libGL
-    libsndfile
-    # libstdcxx5
-    libudev0-shim
-    libudev-zero
     libxkbcommon
     pkg-config
-    unstable.stdenv.cc.cc.lib
     steam-run
     udev
     vulkan-loader

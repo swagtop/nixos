ffb9776348fc8213c056c531d9aaf0daaa38c077
thedb <thedb11@gmail.com>
2025-10-29 09:35:23 +0100

    Adding time to logfile.

--- modules/host-cache.nix
+++ modules/host-cache.nix
@@ -14,13 +14,29 @@
       User = "root";
       WorkingDirectory = "/etc/nixos";
       ExecStart = pkgs.writeShellScript "update-system-flake" ''
-        date '+%Y-%m-%d %H:%M' # For the log.
+        date '+%Y-%m-%d' # For the log.
+        echo "==============="
+
+        echo "$(date '%H:%M') Pulling repository"
+        echo "========================"
+        ${pkgs.git}/bin/git pull --ff-only || echo 'Failed git pull!'; false
         echo
-        ${pkgs.git}/bin/git pull --ff-only || echo 'Failed git pull!'
+
+        echo "$(date '%H:%M') Updating flake inputs"
+        echo "==========================="
         ${pkgs.nix}/bin/nix flake update
+        echo
+
+        echo "$(date '%H:%M') Rebuilding system"
+        echo "======================="
         ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .
+        echo
+
+        echo "$(date '%H:%M') Committing lockfile and pushing"
+        echo "======================="
         ${pkgs.git}/bin/git commit -m "$(date '+%Y-%m-%d') Automatic lockfile update." flake.lock
         ${pkgs.git}/bin/git push
+        echo
       '';
       StandardOutput = "file:/srv/f/cache-log.txt";
       StandardError = "file:/srv/f/cache-log.txt";

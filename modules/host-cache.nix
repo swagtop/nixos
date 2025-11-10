75e1eed66c9c63a5352673c8df29b38111a351ae
thedb <thedb11@gmail.com>
2025-11-10 09:06:58 +0100

    Switching to writeShellApplication for update script in host cache service, should fix issues with git.

--- modules/host-cache.nix
+++ modules/host-cache.nix
@@ -13,36 +13,40 @@
       Type = "oneshot";
       User = "root";
       WorkingDirectory = "/etc/nixos";
-      ExecStart = pkgs.writeShellScript "update-system-flake" ''
-        printf "" > /srv/f/cache-log.txt # Clear log at beginning of service.
+      ExecStart = pkgs.writeShellApplication {
+        name = "update-system-flake";
+        runtimeInputs = with pkgs; [ git nix nixos-rebuild ];
+        text = ''
+          printf "" > /srv/f/cache-log.txt # Clear log at beginning of service.
 
-        echo "$(date '+%Y-%m-%d @ %H:%M') Beginning update"
-        printf "===================================\n\n"
+          echo "$(date '+%Y-%m-%d @ %H:%M') Beginning update"
+          printf "===================================\n\n"
 
-        echo "$(date '+%H:%M') Pulling repository"
-        echo "========================"
-        ${pkgs.git}/bin/git pull --ff-only || echo 'Failed git pull!'; false
-        echo
+          echo "$(date '+%H:%M') Pulling repository"
+          echo "========================"
+          git pull --ff-only || echo 'Failed git pull!'; false
+          echo
 
-        echo "$(date '+%H:%M') Updating flake inputs"
-        echo "==========================="
-        ${pkgs.nix}/bin/nix flake update
-        echo
+          echo "$(date '+%H:%M') Updating flake inputs"
+          echo "==========================="
+          nix flake update
+          echo
 
-        echo "$(date '+%H:%M') Rebuilding system"
-        echo "======================="
-        ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake .
-        echo
+          echo "$(date '+%H:%M') Rebuilding system"
+          echo "======================="
+          nixos-rebuild switch --flake .
+          echo
 
-        echo "$(date '+%H:%M') Committing lockfile and pushing"
-        echo "====================================="
-        ${pkgs.git}/bin/git commit -m "$(date '+%Y-%m-%d') Automatic lockfile update." flake.lock
-        ${pkgs.git}/bin/git push
-        echo
+          echo "$(date '+%H:%M') Committing lockfile and pushing"
+          echo "====================================="
+          git commit -m "$(date '+%Y-%m-%d') Automatic lockfile update." flake.lock
+          git push
+          echo
 
-        echo "$(date '+%H:%M') Finished update"
-        echo "====================="
-      '';
+          echo "$(date '+%H:%M') Finished update"
+          echo "====================="
+        '';
+      };
       StandardOutput = "file:/srv/f/cache-log.txt";
       StandardError = "file:/srv/f/cache-log.txt";
     };

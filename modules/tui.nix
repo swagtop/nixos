8a21e4bc40af490a5f279793c5b1539f7c43bdb9
thedb <thedb11@gmail.com>
2026-05-17 18:20:30 +0200

    Fix y() bash function.

--- modules/tui.nix
+++ modules/tui.nix
@@ -38,7 +38,7 @@ let
     function y() {
     	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
     	command yazi "$@" --cwd-file="$tmp"
-    	IFS= read -r -d '''' cwd < "$tmp"
+    	IFS= read -r -d "" cwd < "$tmp"
     	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
     	command rm -f -- "$tmp"
     }

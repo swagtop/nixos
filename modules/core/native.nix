79e7ce48265c52e694360b11032645273ae6c562
thedb <thedb11@gmail.com>
2026-09-09 20:50:02 +0200

    Make 'pkgsNative' recursively optimize packages.

--- modules/core/native.nix
+++ modules/core/native.nix
@@ -14,21 +14,32 @@ in
 {
   # Run 'gcc -march=native -Q --help=target | grep march' to get march.
   options.swag.native.march = lib.mkOption {
-    type = lib.types.str;
+    type = lib.types.nullOr lib.types.str;
     default = null;
   };
 
   config._module.args.pkgsNative =
     let
       nuPkgs = import inputs.nixpkgs {
+        inherit (pkgs) config;
         inherit (pkgs.stdenv.hostPlatform) system;
-        allowUnfree = true;
       };
     in
     if cfg.march == null then
       pkgs
     else
-      lib.mapAttrs (
-        name: value: if lib.isDerivation value then optimizeForNative value else value
-      ) nuPkgs;
+      let
+        recursiveOptimizeDerivations =
+          set:
+          lib.mapAttrs (
+            name: value:
+            if lib.isDerivation value then
+              optimizeForNative value
+            else if lib.isAttrs value then
+              recursiveOptimizeDerivations value
+            else
+              value
+          ) set;
+      in
+      recursiveOptimizeDerivations nuPkgs;
 }

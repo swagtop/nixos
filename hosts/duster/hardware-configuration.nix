81405ffdd96e290d078f9ad8c30fb31d9e7f290b
thedb <thedb11@gmail.com>
2026-09-10 20:30:08 +0200

    Add march to 'duster' host.

--- hosts/duster/hardware-configuration.nix
+++ hosts/duster/hardware-configuration.nix
@@ -70,6 +70,7 @@
     }
   ];
 
+  swag.native.march = "tigerlake";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
